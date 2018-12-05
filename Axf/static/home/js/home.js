25dec16c14a67abbe1a27cab3fd80d95238b7efe
TMQ6453375 <ganbo6453375@163.com>
2018-12-05 21:09:46 +0800

    首页轮播图

--- Axf/static/home/js/home.js
+++ Axf/static/home/js/home.js
@@ -1,3 +1,15 @@
-/**
- * Created by Administrator on 2018/12/5.
- */
+$(function () {
+
+    console.log('home')
+
+    var topswiper = new Swiper('#top-swiper', {
+        pagination: '.swiper-pagination',
+        nextButton: '.swiper-button-next',
+        prevButton: '.swiper-button-prev',
+        slidesPerView: 1,
+        paginationClickable: true,
+        spaceBetween: 30,
+        loop: true,
+        autoplay : 3000,
+    });
+})
