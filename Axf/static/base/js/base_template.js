d4ea8c59bb7169546ef1a1ec9b4d6c7fe2228293
TMQ6453375 <ganbo6453375@163.com>
2018-12-05 20:16:01 +0800

    项目基本结构完成

---
+++ Axf/static/base/js/base_template.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Administrator on 2018/12/5.
+ */
+$(function () {
+    document.documentElement.style.fontSize = innerWidth / 320 * 16 + 'px'
+
+    console.log(document.documentElement.style.fontSize)
+})
